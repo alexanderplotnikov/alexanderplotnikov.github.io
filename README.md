a53715c23a9846656362b83a04889b8d077a9566
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-02-05 21:42:39 +0200

    calc project upload

--- README.md
+++
@@ -1,8 +0,0 @@
-The Odin Project
-
-WebDev101
-Project: Etch-a-Sketch
-
-Objective: Prompt a user for Grid size
-           Build a grid of div with JavaScript
-           Manipulate DOM to change backgroundColor on Hover
--- main.js
+++ main.js
@@ -1,49 +1,75 @@
-let container = document.querySelector(".container");
-let btn = document.querySelector("button");
-btn.addEventListener("click", () => {
-    let size = prompt("Enter number of columns");
-    let gridSize = document.querySelector(".container");
-    gridSize.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    createGrid(size);
-});
-function createGrid(size){  
-    //to ensure it does not clear at initialization
-    if(container.length != 0){
-        clearGrid();
-    }
-    for(let i = 0; i < size * size; i++){
-        let mouseDown = false;
-        let block = document.createElement("DIV");
-        window.addEventListener("mousedown", (e) => {
-            mouseDown = true;
-        });
-        window.addEventListener("mouseup", (e) => {
-            mouseDown = false;
-        });
-        block.addEventListener("mousemove", (e) => {
-            if (mouseDown == true){
-                block.style.backgroundColor = random_bg_color();
+function add(a, b){
+    return a + b;
+}
+function substract(a, b){
+    return a- b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a / b;
+}
+function operate(num1, num2, sign){
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    switch(sign){
+        case "/":
+            if(num2 == 0){
+                return "cannot divide by 0"
             }
-        });
-        block.addEventListener("click", (e) => {
-            console.log(random_bg_color());
-            block.style.backgroundColor = random_bg_color();         
-        });
-        container.appendChild(block);
-    } 
+            return divide(num1, num2)
+            break;
+        case "+":
+            return add(num1, num2);
+            break;
+        case "*":
+            return multiply(num1, num2);
+            break;
+        case "-":
+            return substract(num1, num2);
+            break;
+        default:
+            return '';
+    }
 }
-function clearGrid(){
-    let container = document.querySelectorAll(".container > div");
-    if(container) {
-        for (let elem of container){
-            elem.remove();
+function updateDisplay(input){
+    display.textContent = input;
+}
+function clear(){
+    input = [''];
+    index = 0;
+    sign = '';
+    updateDisplay(input);
+}
+let input = [''];
+let index = 0;
+let sign = '';
+const btns = document.querySelectorAll("button");
+const display = document.querySelector(".display");
+btns.forEach((btn) => {
+    btn.addEventListener("click", (e) =>{
+        if (e.target.name == "operator"){
+            sign = e.target.value;
+            if(input[index] != ''){
+                index += 1;
+            }
+            input[index] = '';
         }
-    };
-}
-function random_bg_color() {
-    let x = Math.floor(Math.random() * 256);
-    let y = Math.floor(Math.random() * 256);
-    let z = Math.floor(Math.random() * 256);
-    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
-    return bgColor;
-}
+        else {
+            input[index] += e.target.value;
+        }
+        updateDisplay(input[index]);
+    })
+});
+let res;
+const equal = document.querySelector(".equal");
+equal.addEventListener("click", () => {
+    res = operate(input[0], input[1], sign);
+    index = 0;
+    input[0] = res;
+    updateDisplay(res);
+});
+const clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", () => clear());
+
