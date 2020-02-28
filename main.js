d8dd052539ba8e329968ebe7c06d90c23942bf1b
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-02-29 01:36:15 +0200

    uploaded Library project

---
+++ main.js
@@ -0,0 +1,135 @@
+const getForm = document.querySelector("#getForm");
+const form1 = document.getElementById("form_id");
+const submitBtn = document.querySelector("#addBook");
+const cancelBtn = document.querySelector("#cancel");
+const bodyElm = document.querySelector(".container");
+const bookShelf = document.querySelector(".bookShelf");
+
+
+let myLibrary = [];
+
+function Book() {
+    // the constructor
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    let userBook = new Book()
+    myLibrary.push(userBook)
+}
+
+form1.reset(); // resets form entries on Reload
+function updateShelf(){
+    let temp = document.querySelectorAll(".mainBook");
+    for(let j = 0; j < temp.length; j++){
+        temp[j].parentNode.removeChild(temp[j]);
+    }
+}
+function deleteBook(bookIndex){
+    let i = 0;
+    while(bookIndex != i) {i++};
+    return myLibrary.splice(i, 1);    
+}
+function updateRead(bookIndex){
+    let i = 0;
+    while(bookIndex != i) {i++};
+
+    if(myLibrary[i]["read"] == "Read"){
+        myLibrary[i]["read"] = "Not read";
+    }
+    else{
+        myLibrary[i]["read"] = "Read";
+    }
+    return myLibrary;
+}
+let render = function (myLibrary, node) {
+    updateShelf();
+    for (let i = 0; i < myLibrary.length; i++){
+        const bookDiv = document.createElement("DIV");
+        const authorDiv = document.createElement("DIV");
+        const titleDiv = document.createElement("DIV");
+        const slideDiv = document.createElement("DIV");
+        const readDiv = document.createElement("DIV");
+        const pagesDiv = document.createElement("DIV");
+        const deleteBtn = document.createElement("BUTTON");
+        deleteBtn.innerHTML = "X";
+        deleteBtn.classList.add("deleteBtn");
+        deleteBtn.addEventListener("click", () =>{
+            let temp = i;
+            deleteBook(temp);
+            render(myLibrary, bookShelf);
+        });
+        readDiv.addEventListener("click", () =>{
+            let temp = i;
+            updateRead(temp);
+            render(myLibrary, bookShelf);
+        });
+        
+        bookDiv.classList.add("mainBook");
+        authorDiv.classList.add("authorBook");
+        titleDiv.classList.add("titleBook");
+        slideDiv.classList.add("slideBookHidden");
+        readDiv.classList.add("readBook");
+        pagesDiv.classList.add("pagesBook");
+        
+        let template = Object.values(myLibrary[i]);
+        authorDiv.innerHTML = template[0];
+        titleDiv.innerHTML = template[1];
+        pagesDiv.innerHTML = template[2] + "p";
+        readDiv.innerHTML = template[3];
+
+        slideDiv.appendChild(pagesDiv);
+        slideDiv.appendChild(readDiv);
+        slideDiv.appendChild(deleteBtn);
+        bookDiv.appendChild(authorDiv);
+        bookDiv.appendChild(titleDiv);
+        
+        bookDiv.addEventListener("mouseenter", (e) =>{
+            console.log(e.target)
+            slideDiv.classList.toggle("slideBook");
+        });
+        bookDiv.addEventListener("mouseleave", (e) =>{
+            console.log(e.target)
+            slideDiv.classList.toggle("slideBook");
+        });
+        bookDiv.appendChild(slideDiv);
+        
+        node.appendChild(bookDiv);
+    }  
+};
+
+submitBtn.addEventListener("click", () => {
+    author = document.querySelector("#author").value;
+    title = document.querySelector("#title").value;
+    pages = document.querySelector("#pages").value;
+    read = document.querySelector("#read").checked; // returns true or false
+    if (author == '' || title == '' || pages == '') {
+        return alert("Missing Information");
+    }
+    read ? read = "Read" : read = "Not read";
+    addBookToLibrary();
+    changeState();
+    render(myLibrary, bookShelf);
+    
+    console.log(myLibrary)
+});
+
+getForm.addEventListener("click", () => { 
+    changeState();
+});
+cancelBtn.addEventListener("click", () => {
+    changeState();
+});
+bodyElm.addEventListener("click", () => {
+    changeState();
+});
+
+function changeState(){
+    form1.classList.toggle("showForm");
+    bodyElm.classList.toggle("opaque");
+    form1.reset();
+}
+
